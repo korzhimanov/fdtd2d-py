f6c37ea56f2f23decfac465baa5b226d8e8efc51
Artem Korzhimanov <korzhimanov.artem@gmail.com>
2017-02-03 13:47:21 +0300

    Added field generator at left boundary

--- fdtd2d.py
+++ fdtd2d.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import math
+import copy
 
 def calculate_params(init_params):
     """Calculate parameters needed for simulation and return them as a dictionary."""
-    params = init_params.copy()
+    params = copy.deepcopy(init_params)
     
     params['box_size'] = {}
     params['box_size']['x'] = params['x_bounds'][1] - params['x_bounds'][0]
@@ -16,14 +17,37 @@ def calculate_params(init_params):
     
     params['time_step'] = (
             math.sqrt(params['space_step']['x']**2 +
-                      params['space_step']['y']**2)
-            )/2.0
+                      params['space_step']['y']**2))/2.0
     
     params['cfl'] = {}
     params['cfl']['x'] = params['time_step']/params['space_step']['x']
     params['cfl']['y'] = params['time_step']/params['space_step']['y']
     
     return params
+
+def generate_fields_x_min(data, time, params):
+    """Generate fields at left boundary (x_min = params['x_bounds'][0]) at time moment 'time'"""
+    for j in range(params['matrix_size']['y']):
+        data['ey'] [1,j] -= params['laser_pulse_y_shape'](
+                time,
+                params['x_bounds'][0] + params['space_step']['x'],
+                params['y_bounds'][0] + j*params['space_step']['y'])
+        data['ezx'][1,j] -= params['laser_pulse_z_shape'](
+                time + 0.5*params['time_step'],
+                params['x_bounds'][0] + 1.5*params['space_step']['x'],
+                params['y_bounds'][0] + (j-0.5)*params['space_step']['y'])
+        data['ez'] [1,j]  = data['ezx'][1,j] + data['ezy'][1,j]
+    
+    for j in range(params['matrix_size']['y']-1):
+        data['hy'] [1,j] -= params['laser_pulse_z_shape'](
+                time,
+                params['x_bounds'][0] + params['space_step']['x'],
+                params['y_bounds'][0] + j*params['space_step']['y'])
+        data['hzx'][1,j] -= params['laser_pulse_y_shape'](
+                time + 0.5*params['time_step'],
+                params['x_bounds'][0] + 1.5*params['space_step']['x'],
+                params['y_bounds'][0] + (j-0.5)*params['space_step']['y'])
+        data['hz'] [1,j]  = data['hzx'][1,j] + data['hzy'][1,j]
 
 def run(init_params_dict):
     """Run the simulations..."""
--- test_fdtd2d.py
+++ test_fdtd2d.py
@@ -4,6 +4,8 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import copy
+
 import numpy as np
 import h5py
 import fdtd2d
@@ -22,11 +24,24 @@ def params_correct():
     params['matrix_size'] = {'x':64, 'y':64}
     params['time_steps'] = 80
     params['laser_pulse_y_shape'] = laser_pulse_gauss(params['x_bounds'][0], 1., 2.)
-    params['laser_pulse_z_shape'] = lambda t, x, y: 0
+    params['laser_pulse_z_shape'] = laser_pulse_gauss(params['x_bounds'][0], 1., 2.)
     return params
 
-def test_params_calculation(params_correct):
-    p = fdtd2d.calculate_params(params_correct)
+@pytest.fixture
+def params_calculated(params_correct):
+    return fdtd2d.calculate_params(params_correct)
+
+@pytest.fixture
+def data_empty(params_calculated):
+    nx = params_calculated['matrix_size']['x']
+    ny = params_calculated['matrix_size']['y']
+    data = {}
+    for name in ('ex','ey','ezx','ezy','ez','hx','hy','hzx','hzy','hz'):
+        data[name] = np.zeros(shape=(nx,ny), dtype=np.float64)
+    return data
+
+def test_params_calculation(params_calculated):
+    p = params_calculated
     assert(p['box_size']['x'] == 8.0)
     assert(p['box_size']['y'] == 8.0)
     assert(p['space_step']['x'] == 0.125)
@@ -35,6 +50,17 @@ def test_params_calculation(params_correct):
     assert(p['cfl']['x'] == np.sqrt(0.5))
     assert(p['cfl']['y'] == np.sqrt(0.5))
 
+def test_field_generator_x_min(data_empty, params_calculated):
+    d = copy.deepcopy(data_empty)
+    p = params_calculated
+    fdtd2d.generate_fields_x_min(d, 0.0, p)
+    #np.savez_compressed('data_test/field_generator_x_min',
+    #                    ey=d['ey'], hzx=d['hzx'], hz=d['hz'],
+    #                    hy=d['hy'], ezx=d['ezx'], ez=d['ez'])
+    with np.load('data_test/field_generator_x_min.npz') as correct_data:
+        for name in ('ey','hzx','hz','hy','ezx','ez'):
+            assert((d[name] == correct_data[name]).all())
+    
 def test_hdf5(params_correct):
     fdtd2d.run(params_correct)
     assert(os.path.exists("output.hdf5"))
@@ -45,4 +71,4 @@ def test_run(params_correct):
     fdtd2d.run(params_correct)
     assert(os.path.exists("output.hdf5"))
     with h5py.File("output.hdf5", "r") as f:
-        assert(f["ez"] == [[0 for i in range(64)] for j in range(64)])
+        assert(f["ez"] == np.zeros(shape=(64,64), dtype=np.float64))
